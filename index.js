b50926c191fdff7281caa7b06083c2ed7c530465
Matheus Mangueira <98111351+MatheusMangueira@users.noreply.github.com>
2022-04-14 23:20:08 -0300

    at

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
 
-    const idade = document.getElementById("idade").value;
-    const sexo = document.getElementById("sexo").value;
-    const altura = document.getElementById("altura").value
-    const atividade = document.getElementById("atividade").value
-    const peso = document.getElementById("peso").value;
+    const age = document.getElementById("age").value;
+    const sex = document.getElementById("sex").value;
+    const height = document.getElementById("height").value
+    const activity = document.getElementById("activity").value
+    const Weight = document.getElementById("Weight").value;
 
-    const calcular = Math.round(
-        sexo === "masculino"
-            ? (66 + (13.7 * peso) + (5 * altura) - (6.8 * idade))
-            : (665 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade))
+    const calculate = Math.round(
+        sex === "masculino"
+            ? (66 + (13.7 * Weight) + (5 * height) - (6.8 * age))
+            : (665 + (9.6 * Weight) + (1.8 * height) - (4.7 * age))
     );
 
 
-    const manter_peso = (Math.round(calcular * Number(atividade)));
-    const perder_peso = (manter_peso - 450);
-    const ganhar_peso = (manter_peso + 450);
+    const keepWeight = (Math.round(calculate * Number(activity)));
+    const loseWeight   = ( keepWeight - 450);
+    const gainWeight = (keepWeight + 450);
 
 
-    const resultado_final = `
+    const finalResult = `
         
-        <p id="basal">Seu metabolismo basal é de <span> <strong> ${calcular} kcal.</strong> </span></p>
-            <p id="manter">Para manter seu peso você precisa consumir em média <span> <strong> ${manter_peso} kcal.</strong> </span></p>
-            <p id="perder">Para perder peso você precisa consumir em média <span> <strong> ${perder_peso} kcal.</strong> </span></p>
-            <p id="ganhar">Para ganhar peso você precisa consumir em média <span> <strong> ${ganhar_peso} kcal.</strong> </span></p>
+        <p id="basal">Seu metabolismo basal é de <span> <strong> ${calculate} kcal.</strong> </span></p>
+            <p id="manter">Para manter seu peso você precisa consumir em média <span> <strong> ${keepWeight} kcal.</strong> </span></p>
+            <p id="perder">Para perder peso você precisa consumir em média <span> <strong> ${loseWeight} kcal.</strong> </span></p>
+            <p id="ganhar">Para ganhar peso você precisa consumir em média <span> <strong> ${gainWeight} kcal.</strong> </span></p>
         `
 
-    const result = document.getElementById("resultado");
-    result.innerHTML = resultado_final;
+    const result = document.getElementById("result_");
+    result.innerHTML = finalResult;
 
 })
